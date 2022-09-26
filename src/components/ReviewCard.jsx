51d9ef9b6af64d21ff1a719f21eada1828331342
OluC94 <coker.olu94@gmail.com>
2022-09-26 14:49:38 +0100

    sets up bsic structure of review card, ticket 4

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,5 +1,40 @@
 const ReviewCard = () => {
-  return <section className="review-card">Review Card</section>;
+  const tempReview = {
+    review_id: 4,
+    title: "One Night Ultimate Werewolf",
+    category: "hidden-roles",
+    designer: "Akihisa Okui",
+    owner: "happyamy2016",
+    review_body: "We couldn't find the werewolf!",
+    review_img_url:
+      "https://images.pexels.com/photos/5350049/pexels-photo-5350049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    created_at: "2021-01-18T10:01:41.251Z",
+    votes: 5,
+    comment_count: 4,
+  };
+
+  return (
+    <section className="review-card">
+      <h2>{tempReview.title}</h2>
+      <p>Category: {tempReview.category}</p>
+      <p>Author: {tempReview.owner}</p>
+      <img
+        src={tempReview.review_img_url}
+        alt="alt text here"
+        className="review-img"
+      />
+      <p>
+        {tempReview.votes === 1
+          ? tempReview.votes + " vote"
+          : tempReview.votes + " votes"}{" "}
+      </p>
+      <p>
+        {tempReview.comment_count === 1
+          ? tempReview.comment_count + " comment"
+          : tempReview.comment_count + " comments"}
+      </p>
+    </section>
+  );
 };
 
 export default ReviewCard;
