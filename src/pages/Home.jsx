8ae17e4ba633af28d1fcff146748cb54f8c843ac
OluC94 <coker.olu94@gmail.com>
2023-01-31 15:04:17 +0000

    re-deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <section className="main-page">Welcome to NC Games!</section>;
+  return (
+    <section className="main-page">
+      Welcome to NC Games!
+      <br></br>
+      <span>Please select "Categories" or "Reviews" to explore</span>
+    </section>
+  );
 };
 
 export default Home;
